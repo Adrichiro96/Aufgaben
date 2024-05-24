085b3fe83d1de30ae4ba9e5eee9b83d9324b5130
Adrian Chinchilla <168095537+Adrichiro96@users.noreply.github.com>
2024-05-24 16:30:00 +0200

    Aufgabe 2 .dart

--- Aufgabe 2 .dart
+++ Aufgabe 2 .dart
@@ -1,9 +1,8 @@
 void main() {
-  // Aufgabe 2: For-Schleife mit Summation
   print("\nAufgabe 2:");
   int sum = 0;
   for (int i = 1; i <= 100; i++) {
     sum += i;
   }
-  print("Summe von 1 bis 100: $sum");
+  print("Summ from 1 to 100: $sum");
 }
